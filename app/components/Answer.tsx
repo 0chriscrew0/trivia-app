1323df5933ed59c80dd28c27c46c2a3b8d20b36e
Christopher Crow <0chriscrew0@gmail.com>
2023-03-01 13:26:06 -0600

    edit Answer component styles

--- app/components/Answer.tsx
+++ app/components/Answer.tsx
@@ -8,7 +8,7 @@ type AnswerProps = {
 
 export default function Answer({ answer }: AnswerProps) {
   return (
-    <button className='text-center border-2 py-2 hover:bg-green-400 hover:drop-shadow-lg hover:scale-105'>
+    <button className='text-center border-2 py-2 hover:drop-shadow-lg hover:scale-105 hover:border-0 hover:bg-gray-200'>
       {decode(answer)}
     </button>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ const roboto = Roboto({
 
 export default function Home() {
   return (
-    <main className={`${roboto.className}`}>
+    <main className={`${roboto.className} bg-gray-200`}>
       <QuestionLoader />
     </main>
   );
