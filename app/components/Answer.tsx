9546f1e50540527d3b787389d94cb75ecd7c2369
Christopher Crow <0chriscrew0@gmail.com>
2023-03-23 12:46:51 -0500

    add functionality to detect correct answer on click

--- app/components/Answer.tsx
+++ app/components/Answer.tsx
@@ -4,11 +4,15 @@ import { decode } from "html-entities";
 
 type AnswerProps = {
   answer: string;
+  gradeQuestion: (userAnswer: string) => void;
 };
 
-export default function Answer({ answer }: AnswerProps) {
+export default function Answer({ answer, gradeQuestion }: AnswerProps) {
   return (
-    <button className='text-center border-2 py-2 hover:drop-shadow-lg hover:scale-105 hover:border-0 hover:bg-gray-200'>
+    <button
+      onClick={() => gradeQuestion(answer)}
+      className='text-center border-2 py-2 hover:drop-shadow-lg hover:scale-105 hover:border-0 hover:bg-gray-200'
+    >
       {decode(answer)}
     </button>
   );
--- app/components/Question.tsx
+++ app/components/Question.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import Answer from "./Answer";
 
 type QuestionProps = {
@@ -13,14 +14,24 @@ export default function Question({
   answer,
   incorrectAnswers,
 }: QuestionProps) {
+  const [grade, setGrade] = useState(false);
   const allAnswers = [...incorrectAnswers, answer];
   allAnswers.sort(() => Math.random() - 0.5);
+
+  const gradeQuestion = (userAnswer: string) => {
+    if (userAnswer === answer) {
+      setGrade(true);
+    }
+    return;
+  };
+
   return (
     <div className='mt-16 mx-auto bg-white border-2 px-8 pt-16'>
       <h1 className='text-center mb-8'>{question}</h1>
+      {grade && <p>Correct!</p>}
       <div className='my-16 flex flex-col gap-8'>
         {allAnswers.map((answer, i) => (
-          <Answer key={i} answer={answer} />
+          <Answer key={i} answer={answer} gradeQuestion={gradeQuestion} />
         ))}
       </div>
     </div>
